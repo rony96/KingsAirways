12200c17c98e4312d821a59d0f2eff55426242ed
ShafiBhaii <mohamedshafi1996@gmail.com>
2018-08-21 22:32:20 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
 import { UserModule } from './user/user.module';
 import {AdminModule} from './admin/admin.module';
+import { HeaderComponent } from './ui/header.component';
+import { FooterComponent } from './ui/footer.component';
+import { UserdashboardComponent } from './user/userdashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserdashboardComponent
   ],
   imports: [
     BrowserModule, UserModule,AdminModule,
